c46cfb50aee36e302922bd5045ee800567a0d9bd
JoshuaLin0903 <b06901016@ntu.edu.tw>
2019-12-12 16:38:16 +0800

    get_phi done

--- campus_test/campus_test.ino
+++ campus_test/campus_test.ino
@@ -5,25 +5,28 @@ displays them in serial monitor.
 */
 
 #include <MPU9255.h>//include MPU9255 library
-#include <math.h>
 
 MPU9255 mpu;
+//L298N
+#define MotorL_I1     12  //定義 I1 接腳（左）
+#define MotorL_I2     6  //定義 I2 接腳（左）
+#define MotorR_I3     9  //定義 I3 接腳（右）
+#define MotorR_I4     10 //定義 I4 接腳（右）
+#define MotorL_PWML   3  //定義 ENA (PWM調速) 接腳
+#define MotorR_PWMR   5  //定義 ENB (PWM調速) 接腳
 
-void get_maxmin(int16_t& x_max, int16_t& x_min, int16_t& y_max, int16_t& y_min){
-  mpu.read_mag();
-}
+// 循線模組
+#define L1  A0  // Define Second Right Sensor Pin
+#define L2  A1  // Define First Right Sensor Pin
+#define M   A2  // Define Middle Sensor Pin
+#define R2  A3  // Define First Left Sensor Pin
+#define R1   2  // Define Second Leftt Sensor Pin
 
-float get_phi(int16_t x_max, int16_t x_min, int16_t y_max, int16_t y_min){
-  mpu.read_mag();
-  float x, phi, x_offset, y_offset;
-  x_offset = (x_max + x_min)/2;
-  x = mpu.mx - x_offset;
-  if(abs(x) > abs(x_offset)){ x = (float(x_offset)) * (x > 0 ? 1 : -1); }
-
-  if(mpu.my > (y_max + y_min)/2){  }
-  phi = acos(x/x_offset)*180/PI;
-  return phi;
-}
+// initalize parameter
+int r2=0,r1=0,m=0,l1=0,l2=0;
+int _Tp=80;
+double _rl_ratio = 1.33; // to match the motor on straight line
+bool adj = true;
 
 void setup() {
   Serial.begin(115200);//initialize Serial port
@@ -36,21 +39,51 @@ void setup() {
   {
   Serial.println("initialization successful!");
   }
+   //L298N pin
+   pinMode(MotorL_I1,   OUTPUT);
+   pinMode(MotorL_I2,   OUTPUT);
+   pinMode(MotorR_I3,   OUTPUT);
+   pinMode(MotorR_I4,   OUTPUT);
+   pinMode(MotorL_PWML, OUTPUT);
+   pinMode(MotorR_PWMR, OUTPUT);
+   //tracking pin
+   pinMode(R1, INPUT); 
+   pinMode(R2, INPUT);
+   pinMode(M,  INPUT);
+   pinMode(L1, INPUT);
+   pinMode(L2, INPUT);
 
 }
 
+#include "track.h"
+#include "compass.h"
+#include "node.h"
+
 void loop() {
-  mpu.read_mag();//get data from the magnetometer
-  int16_t x_max, x_min, y_max, y_min;
-  
-  Serial.print("MX: ");
-  Serial.print(mpu.mx);
-  Serial.print(" MY: ");
-  Serial.print(mpu.my);
-  Serial.print(" MZ: ");
-  Serial.print(mpu.mz);
-  Serial.print("   Phi: ");
-  Serial.println(get_phi(-25, -360, 440, 100));
-  
-  delay(100);
+  if(adj){
+    get_maxmin();
+    adj = false;
+    delay(5000);
+  }
+  else
+  {
+    Serial.print("Phi:");
+    Serial.println(get_phi());
+    delay(1000);
+    /*
+    r1 = digitalRead(R1); // right-outer sensor
+    r2 = digitalRead(R2); // right-inner sensor
+    m  = digitalRead(M);  // middle sensor
+    l2 = digitalRead(L2); // left-inner sensor
+    l1 = digitalRead(L1); // left-outer sensor
+    if(r1 == 1 && l1 == 1)
+    {
+        fiveBlack_adjest();
+        left_turn();
+    }
+    else
+    {
+        tracking(r2,r1,m,l1,l2);
+    }*/
+  }
 }
